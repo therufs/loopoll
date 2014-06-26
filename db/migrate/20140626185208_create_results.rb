b69b531c0cfc9fcfd0ae1968e73fc09b325f9ad1
maebeale <maebeale@gmail.com>
2014-06-26 15:21:13 -0400

    edited create table migrations with fields and references

--- db/migrate/20140626185208_create_results.rb
+++ db/migrate/20140626185208_create_results.rb
@@ -1,6 +1,8 @@
 class CreateResults < ActiveRecord::Migration
   def change
     create_table :results do |t|
+      t.references :user, index: true
+      t.references :answer, index: true
 
       t.timestamps
     end
