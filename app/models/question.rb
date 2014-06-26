1b006b07ec20749ff2149d7bcbd028c9b8c11b2a
maebeale <maebeale@gmail.com>
2014-06-26 15:05:55 -0400

    added associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
+  has_many :results, :through => :user
+  has_many :answers
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,6 @@
 class Result < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :answer
+  belongs_to :question, :through => :user
+
 end
