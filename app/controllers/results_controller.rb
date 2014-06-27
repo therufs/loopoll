cdc930f8a3cc9ad4567b1e3ddcb0f49ed9659a42
Sebastian Armano <sebarmano@gmail.com>
2014-06-27 09:28:34 -0400

    Added tables to the questions index view

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -69,6 +69,6 @@ class ResultsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def result_params
-      params[:result]
+      params.require(:result).permit(:user_id, :answer_id)
     end
 end
