162f93a502c8113192dfec2f8af1131b77582eb9
maebeale <maebeale@gmail.com>
2014-06-28 21:38:19 -0400

    edited seeds and deleted dupe migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626185601) do
+ActiveRecord::Schema.define(version: 20140627160041) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140626185601) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "questions", force: true do |t|
     t.string   "content"
