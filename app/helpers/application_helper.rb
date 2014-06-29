9c6b8956f8fc45ed1b0153e9a1d06fba1e9835b0
Sebastian Armano <sebarmano@gmail.com>
2014-06-29 10:56:17 -0400

    Added style to question show and question new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def card(data, text, pos = "")
+      content_tag :div, class: "large-3 columns #{pos}" do
+        content_tag :div, class: "panel radius card" do
+          content_tag(:h2, data) +
+          content_tag(:p, text)
+        end
+      end
+    end
 end
