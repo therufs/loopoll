38eb190f62024257219aa1b186645eb69f1842ac
Sebastian Armano <sebarmano@gmail.com>
2014-06-29 17:14:38 -0400

    Added verification of login for accessing pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
 
   helper_method :current_user
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path
+    end
+  end
+
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+
 
 end
