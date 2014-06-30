b829ca7eae81ef855c6a50b206102cf47da76bae
Sebastian Armano <sebarmano@gmail.com>
2014-06-29 20:38:40 -0400

    added methods to questions and result

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,12 @@
 class Result < ActiveRecord::Base
-  validates_uniqueness_of :user_id, scope: :answer_id
-
+  validate :uniqueness_of_result
   belongs_to :user
   belongs_to :answer
+
+  # def uniqueness_of_result
+  #   if user
+  # end
+
+
+
 end
